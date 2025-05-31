9d239c6fe6505fe26f44493e7c77c6968655abf3
Marcos Marques <mdmarques70@gmail.com>
2025-05-31 17:02:11 -0300

    Add nvim config

---
+++ .config/nvim/lua/plugins/coloscheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- add gruvbox
+  { "rebelot/kanagawa.nvim" },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  }
+}
