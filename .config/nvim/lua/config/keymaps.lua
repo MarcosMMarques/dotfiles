6f2aa6f391600dbf9bcdf819136c6f41d98cb2dc
Marcos Marques <mdmarques70@gmail.com>
2025-06-01 17:05:28 -0300

    Add kitty config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,6 @@
 
 vim.cmd([[map <C-a> ggVG ]])
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+vim.keymap.set("i", "<C-h>", "<Left>")
+vim.keymap.set("i", "<C-l>", "<Right>")
+vim.keymap.set("i", "<C-j>", "<Down>")
